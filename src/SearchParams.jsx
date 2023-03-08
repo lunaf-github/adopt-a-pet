83c026590c77e83848e406c6b60284c5009bd6a0
lunaf-github <lunaf91@gmail.com>
2023-03-07 18:14:24 -0800

    Add useBreedList hook  and Results componenet

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect}  from 'react';
 import Pet from './Pet';
+import useBreedList from './useBreedList';
+import Results from './Results';
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -7,7 +9,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
@@ -50,7 +52,7 @@ const SearchParams = () => {
         <label htmlFor='bread'>
           Breed
           <select 
-            id="bread"
+            id="breed"
             disabled ={noOptions(breeds)} 
             value={breed}
             onChange={updateBreed}
@@ -61,8 +63,8 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
 
-      {pets.map(generatePets)}
     </div>
   );
 
