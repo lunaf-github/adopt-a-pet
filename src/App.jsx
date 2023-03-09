32f67897d44d1c7ecf3cecdabba799acb7bddbf5
lunaf-github <lunaf91@gmail.com>
2023-03-08 18:04:53 -0800

    Add react query to cache fetch results

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity
+    },
+  },
+});
+
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt me!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
